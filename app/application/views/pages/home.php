88b260e2945f2542ca3da5ba4f4abca6a90ca19f
Killian Murphy <klcm500@york.ac.uk>
2014-03-04 22:48:46 +0000

    Styled and added responsiveness to home page. Still need to figure out what's up what's down what's going around with the search results and search bar in mobile-mode.

--- app/application/views/pages/home.php
+++ app/application/views/pages/home.php
@@ -1,12 +1,19 @@
 <h2 id="recently_viewed_recipes_title">Recently Viewed Recipes</h2>
 <div id="recently_viewed_recipes">
-    Here be recently viewed recipes.
     <!-- Here as many recently viewed items as the controller informs this view
          of will be passed to the recently viewed template to be rendered. -->
+    <div id="recently_viewed_grid">
+        <? for ($i = 1; $i < 4; $i++) {
+            echo "<div class=\"recently_viewed_recipe\"><img class =\"recently_viewed_recipe_image\" src=\"http://is.gd/MLNCn7\"/><h3 class=\"recently_viewed_recipe_name\">Recipe</h3></div>";
+        } ?>
+    </div>
 </div>
 <h2 id="categorical_browse_title">Browse Recipes By Category</h2>
 <div id="categorical_recipe_browse">
-    <? for ($i = 1; $i < 10; $i++) {echo "<div class=\"category_element\"><a href=\"\">Category ".$i."</a></div>";}?>
-
+    <div id="category_grid">
+        <? for ($i = 1; $i < 10; $i++) {
+            echo "<div class=\"recipe_category\"><img class =\"recipe_category_image\" src=\"http://is.gd/MLNCn7\"/><h3 class=\"recipe_category_name\">Category</h3></div>";
+        } ?>
+    </div>
 </div>
 <br/>
