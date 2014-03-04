d89d6df415bfa47f5410662e8fd42eafd7640e9f
Killian Murphy <klcm500@york.ac.uk>
2014-03-04 18:01:29 +0000

    Changed recipe page to an attempt at responsive. Need to style the rest of the pages, and figure out what to do with search when using a viewport of x320 or less.

--- app/application/views/templates/header.php
+++ app/application/views/templates/header.php
@@ -6,21 +6,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="<? echo base_url()."assets/styles.css"; ?>" rel="stylesheet" type="text/css">
-    <script src="<? echo base_url()."assets/jquery.js"; ?>" type="text/javascript"></script>
-    <script src="<? echo base_url()."assets/recipe.js"; ?>" type ="text/javascript"></script>
+    <link href="<? echo base_url() . "assets/styles.css"; ?>" rel="stylesheet" type="text/css">
+    <script src="<? echo base_url() . "assets/jquery.js"; ?>" type="text/javascript"></script>
+    <script src="<? echo base_url() . "assets/recipe.js"; ?>" type="text/javascript"></script>
 </head>
 <body>
-<div id="banner">
-    <div id="banner_text">
-    <h1><?php echo $title ?></h1>
-    </div>
+<div id="container">
+    <div id="banner">
+        <div id="banner_text">
+            <h1><?php echo $title ?></h1>
+        </div>
 
-    <div id="search">
-        <form>
-            <input id="search_input" name="search_input" placeholder="Enter Search Query...">
-            <input id="search_submit"  type="submit" value="Search">
-        </form>
+        <div id="search">
+            <form>
+                <input id="search_input" name="search_input" placeholder="Enter Search Query...">
+                <input id="search_submit" type="submit" value="Search">
+            </form>
+        </div>
     </div>
-</div>
 
