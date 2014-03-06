95567e97ad51f4f14738b727590f55c0b7aa5d3b
Killian Murphy <klcm500@york.ac.uk>
2014-03-06 21:17:55 +0000

    Styled the search results page and template.

--- app/application/views/pages/search.php
+++ app/application/views/pages/search.php
@@ -1,8 +1,13 @@
-<div id="search_results">Search results go here:
+<div id="search_results">
+    <h2>Search Results:</h2>
     <?
       // Here as many result objects as were created by the search query or
       // application of a filter will be passed to the
       // 'views/templates/search_result.php' template to be rendered.
+    for ($i = 1; $i < 6; $i++) {
+        $recipe=['name' => "Recipe ".$i, 'difficulty' => "Mega", 'time' => "Forever"];
+        $this->load->view("templates/search_result", $recipe);
+    }
     ?>
 
 </div>
--- app/application/views/templates/search_result.php
+++ app/application/views/templates/search_result.php
@@ -1,10 +1,10 @@
 <div class="search_result">
     <!--  The image src and recipe attributes will be dynamically set from
           the model data passed to the controller. -->
-    <img class="search_result_image" src="http://is.gd/4ryd2A"/>
+    <img class="search_result_image" src="http://is.gd/OZuAPp"/>
     <ul class="search_result_attributes">
-        <li>Recipe Name</li>
-        <li>Recipe Difficulty</li>
-        <li>Recipe Cooking Time</li>
+        <li><b>Name:</b> <? echo $name; ?></li>
+        <li><b>Difficulty:</b> <? echo $difficulty ?></li>
+        <li><b>Cooking Time:</b> <? echo $time; ?></li>
     </ul>
 </div>
