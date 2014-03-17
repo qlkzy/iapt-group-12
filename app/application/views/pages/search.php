e97cd40db2ea556f25598f36dc02e42c5ed87876
Killian Murphy <klcm500@york.ac.uk>
2014-03-17 16:40:11 +0000

    Almost there with search page reboot - need to decide on what filters will be used and what information will be displayed on recipe cards.

--- app/application/views/pages/search.php
+++ app/application/views/pages/search.php
@@ -3,19 +3,29 @@
     <div id="search_results">
         <!-- <h3>Search Results:</h3> -->
         <div class="search_result">
-            <img class="srch_res_img" src="<?php echo base_url()."assets/images/bburgers.jpeg"; ?>"/>
+            <img class="srch_res_img" src="<?php echo base_url() . "assets/images/bburgers.jpeg"; ?>"/>
             <h4 class="srch_res_title">Beef Burgers</h4>
         </div>
         <div class="search_result">
-            <img class="srch_res_img" src="<?php echo base_url()."assets/images/bburgers.jpeg"; ?>"/>
+            <img class="srch_res_img" src="<?php echo base_url() . "assets/images/bburgers.jpeg"; ?>"/>
             <h4 class="srch_res_title">Beef Burgers</h4>
         </div>
         <div class="search_result">
-            <img class="srch_res_img" src="<?php echo base_url()."assets/images/bburgers.jpeg"; ?>"/>
+            <img class="srch_res_img" src="<?php echo base_url() . "assets/images/bburgers.jpeg"; ?>"/>
+            <h4 class="srch_res_title">Beef Burgers</h4>
+        </div>
+        <div class="search_result">
+            <img class="srch_res_img" src="<?php echo base_url() . "assets/images/bburgers.jpeg"; ?>"/>
             <h4 class="srch_res_title">Beef Burgers</h4>
         </div>
     </div>
     <div id="search_filters">
         <h3>Filter Results</h3>
+        <ul id="filter_list">
+            <?php for ($i = 1; $i < 6; $i++) {
+                echo "<li>Filter".$i."</li>";
+            }
+            ?>
+        </ul>
     </div>
 </div>
