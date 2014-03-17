6bc6a32f3b0b9a17af6d0ae61d4f3b8c4221bcbb
Killian Murphy <klcm500@york.ac.uk>
2014-03-17 23:35:33 +0000

    Filters done. Need to do cards then backend.

--- app/application/views/pages/home.php
+++ app/application/views/pages/home.php
@@ -18,3 +18,15 @@
     </div>
 </div>
 <br/> -->
+<div id="home_content">
+    <a class="home_box" href="#" id="home_gs">
+        <h2 class="home_bhead">Get Started!</h2>
+        <i class="fa fa-check fa-5x home_bicon"></i>
+        <p class="home_btext">Set your default recipe view and start checking out some recipes.</p>
+    </a>
+    <a class ="home_box" href="<?php echo base_url()."help"; ?>" id="home_vh">
+        <h2 class="home_bhead">Call D. Hull</h2>
+        <i class="fa fa-question fa-5x home_bicon"></i>
+        <p class="home_btext">Find out how you can use this app.</p>
+    </a>
+</div>
--- app/application/views/pages/search.php
+++ app/application/views/pages/search.php
@@ -21,11 +21,54 @@
     </div>
     <div id="search_filters">
         <h3>Filter Results</h3>
-        <ul id="filter_list">
-            <?php for ($i = 1; $i < 6; $i++) {
-                echo "<li>Filter".$i."</li>";
-            }
-            ?>
-        </ul>
+
+        <form action="#" id="search_fform" name="search_fform">
+            <ul>
+                <li>
+                    <label for="search_fcat">Dish Type:</label>
+                    <select id="search_fcat" form="search_fform" name="search_fcat">
+                        <option value="" disabled selected>----------------</option>
+                        <option value="main">Main</option>
+                        <option value="side">Side</option>
+                        <option value="salad">Salad</option>
+                        <option value="dessert">Dessert</option>
+                    </select>
+                </li>
+                <li>
+                    <label for="search_fdiet">Dietary Restrictions:</label>
+                    <select id="search_fdiet" form="search_fform" name="search_fdiet">
+                        <option value="" disabled selected>----------------</option>
+                        <option value="vegetarian">Vegatarian</option>
+                        <option value="vegan">Vegan</option>
+                        <option value="kosher">Kosher</option>
+                        <option value="hala">Halal</option>
+                    </select>
+                </li>
+                <li>
+                    <label for="search_fdif">Difficulty:</label>
+                    <select id="search_fdif" form="search_fform" name="search_fdif">
+                        <option value="" disabled selected>----------------</option>
+                        <option value="beginner">Beginner</option>
+                        <option value="intermediate">Intermediate</option>
+                        <option value="advanced">Advanced</option>
+                    </select>
+                </li>
+                <li>
+                    <label for="search_fctime">Time (up to):</label>
+                    <input form="search_fform" id="search_fctime" maxlength="2" name="search_fctime" pattern="[0-9][0-9]" placeholder="minutes" size="2" type="text" />
+                </li>
+                <li>
+                    <label for="search_fserves">Serves:</label>
+                    <input form="search_fform" id="search_fserves" maxlength="2" name="search_fserves" pattern="[0-9][0-9]" placeholder="people" size="2" type="text"/>
+                </li>
+                <li>
+                    <label for="search_fcontains">Contains:</label>
+                    <input form="search_fform" id="search_fcontains" name="search_fcontains" pattern="[a-zA-Z]+" placeholder="ingredient" size="8" type="text"/>
+                </li>
+                <li>
+                    <input form="search_fform" name="submit" type="submit" value="submit"/>
+                </li>
+            </ul>
+        </form>
     </div>
 </div>
