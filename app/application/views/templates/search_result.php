21312c8181b4c260ef120037240756b9f896c201
Killian Murphy <klcm500@york.ac.uk>
2014-03-22 15:32:04 +0000

    Meddling. Push to switch to laptop.

--- app/application/views/templates/search_result.php
+++ app/application/views/templates/search_result.php
@@ -2,5 +2,10 @@
     <div class="search_result" data-category="<?php echo strtolower($recipe->getCategory()); ?>" data-difficulty="<?php echo $recipe->getDifficulty(); ?>" data-time="<?php echo $recipe->getCookingTime(); ?>" data-serves="<?php echo $recipe->getServes(); ?>">
         <img class="srch_res_img" src="<?php echo $recipe->getImage(); ?>"/>
         <h4 class="srch_res_title"><?php echo $recipe->getName(); ?></h4>
+        <ul>
+            <li><b>Category: </b><?php echo $recipe->getCategory(); ?></li>
+            <li><b>Difficulty: </b><?php echo $recipe->getDifficulty(); ?></li>
+            <li><b>Cooking time: </b><?php echo $recipe->getCookingTime()." minutes"; ?></li>
+        </ul>
     </div>
 </a>
