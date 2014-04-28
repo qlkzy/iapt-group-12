031e56bf952351711a619cd9ea560700b9e3b5e8
David Morris <davidrowlandmorris@gmail.com>
2014-04-28 19:09:54 +0100

    Only show welcome page until the user selects a default view - fixes #50

--- app/application/controllers/home.php
+++ app/application/controllers/home.php
@@ -5,6 +5,11 @@ class Home extends CI_Controller
     public function index() {
         $data['title'] = "Home";
 
+        $defaultView = $this->session->userdata('defaultView');
+        if (!empty($defaultView)) {
+            header('Location: ' . base_url('index.php/search'));
+        }
+
         $this->load->view("templates/header", $data);
         $this->load->view("pages/home", $data);
         $this->load->view("templates/footer", $data);
