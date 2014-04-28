c206107b94ffb03f17a918de0883022b89960214
David Morris <davidrowlandmorris@gmail.com>
2014-04-28 23:04:15 +0100

    Fix reset button

--- app/assets/scripts/search.js
+++ app/assets/scripts/search.js
@@ -1,44 +1,3 @@
-// TODO: Add the ability to filter by ingredient / dietary restriction.
-
-function reset() {
-    $(".search_result").each(function () {
-        $(this).show();
-    });
-}
-
-/* function filter(property, values) {
- if (value) {
- $(".search_result[data-" + property + "!='" + value + "']").hide();
- }
- }
- */
-
-function filter(values) {
-    console.log(values.toString());
-    var category = values[0];
-    var dietary = values[1];
-    var difficulty = values[2];
-    var time = values[3];
-    var serves = values[4];
-
-    var test = ".search_result" + (category ? ("[data-category='" + category + "']") : ("")) + (dietary ? ("[data-dietary='" + dietary + "']") : ("")) + (difficulty ? ("[data-difficulty='" + difficulty + "']") : ("")) + (time ? ("[data-time='" + time + "']") : ("")) + (serves ? ("[data-serves='" + serves + "']") : (""));
-    $(test).show();
-    $(".search_result").not(test.slice(14)).hide();
-
-
-}
-
-function filterTime(property, value) {
-
-    if (value) {
-        $(".search_result").each(function () {
-            if (parseInt($(this).attr("data-" + property)) > value) {
-                $(this).hide();
-            }
-        });
-    }
-}
-
 $(document).ready(function () {
     $('select[data-selected]').each(function (entry) {
         var choice = $(this).data('selected');
@@ -55,5 +14,9 @@ $(document).ready(function () {
         $(this).submit();
     });
 
-    $("#search_fform_reset").click(reset);
+    $("#search_fform_reset").click(function () {
+        $('#search_fform * input').val('');
+        $('#search_fform * select').val(0);
+        $('#search_fform').change();
+    });
 });
