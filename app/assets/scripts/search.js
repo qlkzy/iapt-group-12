44c2367634df4b1eb4b256bb2d0012b6483596f0
David Morris <davidrowlandmorris@gmail.com>
2014-04-28 23:17:09 +0100

    Emphasise filter box when filter elements are focused

--- app/assets/scripts/search.js
+++ app/assets/scripts/search.js
@@ -19,4 +19,12 @@ $(document).ready(function () {
         $('#search_fform * select').val(0);
         $('#search_fform').change();
     });
+
+    $("#search_fform * input, #search_fform * select").focus(function () {
+        $('#search_filters').addClass('focused');
+    })
+
+    $("#search_fform * input, #search_fform * select").blur(function () {
+        $('#search_filters').removeClass('focused');
+    });
 });
